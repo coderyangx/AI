ad2f0b54c545b4a409fbd1e17074ff73b5636744
coderyangx <993794142@qq.com>
2025-07-01 13:21:43 +0800

    feat: add dep

--- packages/server/types/index.d.ts
+++ packages/server/types/index.d.ts
@@ -2,9 +2,9 @@ declare module '@hono/node-server' {
   export function serve(options: any, callback?: (info: any) => void): any;
 }
 
-declare module '@ai-sdk/openai' {
-  export function createOpenAI(options: any): any;
-}
+// declare module '@ai-sdk/openai' {
+//   export function createOpenAI(options: any): any;
+// }
 
 declare module 'winston' {
   export const createLogger: any;
